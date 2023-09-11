e715e0652f3892b136622558e4ff817b131d76e1
Maksym-Bohun <maks.boggun@gmail.com>
2023-09-11 16:16:27 +0300

    created basic app

---
+++ analytics.js
@@ -0,0 +1,24 @@
+const createAnalytics = () => {
+  let counter = 0;
+  let isDestroyed = false;
+  const listener = () => {
+    counter++;
+  };
+
+  document.addEventListener("click", listener);
+
+  return {
+    destroy() {
+      document.removeEventListener("click", listener);
+      isDestroyed = true;
+    },
+    getClicks() {
+      if (isDestroyed) {
+        return "Analytics destroyed!";
+      }
+      return counter;
+    },
+  };
+};
+
+window.analytics = createAnalytics();
---
+++ index.js
@@ -0,0 +1,3 @@
+const post = new Post("Webpack post title");
+
+console.log("Post toString ", post.toString());
